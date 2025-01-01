98ceb3978667481a9d221d61d91fc8d982390584
limoozyn <limoozyn@gmail.com>
2025-01-01 13:19:31 +0200

    add next config js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    mongodb_username: process.env.MONGODB_USERNAME,
+    mongodb_password: process.env.MONGODB_PASSWORD,
+    mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+    mongodb_database: process.env.MONGODB_DATABASE,
+  },
+};
